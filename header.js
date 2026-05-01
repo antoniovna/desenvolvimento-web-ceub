7fc5ed6d5a2ec137dca7a3a05ca79b65c35dd35b
Antônio Vinicius Nunes de Alencar <antonio.alencar@dinfor.uniceub>
2026-04-30 22:01:28 -0300

    adicionando pagina do fotografo

--- header.js
+++ header.js
@@ -1,17 +1,18 @@
 const headerHTML = `
     <nav class="hub-navbar">
         <ul>
-            <li><a href="index.html" id="nav-inicio">Início</a></li>
-            <li><a href="quem-sou-eu.html" id="nav-quem-sou-eu">Quem sou eu?</a></li>
+            <li><a href="./index.html" id="nav-inicio">Início</a></li>
+            <li><a href="./quem-sou-eu.html" id="nav-quem-sou-eu">Quem sou eu?</a></li>
             <li class="dropdown">
-                <a href="aulas.html" class="dropbtn" id="nav-aulas">Aulas</a>
+                <a href="./aulas.html" class="dropbtn" id="nav-aulas">Aulas</a>
                 <div class="dropdown-content">
-                    <a href="relatorios/Aula%201/">Introdução ao HTML</a>
-                    <a href="relatorios/Aula%202/fabrica_de_atletas.html">Listas e Forms</a>
-                    <a href="relatorios/Aula%203/">Menu, Imagens</a>
-                    <a href="relatorios/Aula%204/">Cards, Audio players</a>
-                    <a href="relatorios/Aula%207/index.html">Landing Page GameTECH</a>
-                    <a href="relatorios/Aula%208/index.html">Tabela Periódica</a>
+                    <a href="./relatorios/Aula%201/">Introdução ao HTML</a>
+                    <a href="./relatorios/Aula%202/fabrica_de_atletas.html">Listas e Forms</a>
+                    <a href="./relatorios/Aula%203/">Menu, Imagens</a>
+                    <a href="./relatorios/Aula%204/">Cards, Audio players</a>
+                    <a href="./relatorios/Aula%207/index.html">Landing Page GameTECH</a>
+                    <a href="./relatorios/Aula%208/index.html">Tabela Periódica</a>
+                    <a href="./relatorios/Aula%209/index.html">Fotógrafo</a>
                 </div>
             </li>
             <li><a href="relatorios.html" id="nav-relatorios">Relatórios</a></li>
